d0b601ce441f88646a0bda6f4fe22bcec82b5678
Parfait G <parfait.gasana@gmail.com>
2018-02-01 20:21:16 -0600

    Updated for R-specific solution

--- XSLTransform_R.R
+++ XSLTransform_R.R
@@ -1,49 +1,14 @@
-library(XML)
+library(xslt)
 
-#######################################################
-### LINUX USERS (SXSLT MODULE)
-#######################################################
-library(Sxslt)   # AVAILABLE ON http://www.omegahat.org/Sxslt/
+doc <- read_xml("/path/to/Input.xml", package = "xslt")
+style <- read_xml("/path/to/Script.xsl", package = "xslt")
+new_xml <- xml_xslt(doc, style)
 
-xmlfile = '/path/to/Input.xml'
-xslfile = "/path/to/XSLT_Script.xsl"
-newxmlfile = "/path/to/Output.xml"
+# OUTPUT TO SCREEN
+cat(as.character(new_xml))
 
-doc <- xsltApplyStyleSheet(xmlfile, xslfile, isURL= FALSE)
-
-saveXML(doc, file=newxmlfile)
-
-
-#######################################################
-### WINDOWS USERS (RDOMCLIENT MODULE AND MSXML OBJECT)
-#######################################################
-library(RDCOMClient)
-
-# INITIATE OBJECTS
-xmlobj = COMCreate("MSXML2.DOMDocument")
-xslobj = COMCreate("MSXML2.DOMDocument")
-newxmlobj = COMCreate("MSXML2.DOMDocument")
-
-xmlfile = 'C:\\Path\\To\\Input.xml'
-xslfile = "C:\\Path\\To\\XSLT_Script.xsl"
-newxmlfile = "C:\\Path\\To\\Output.xml"
-
-# LOADING XML & XSLT FILES
-xmlobj.async = FALSE
-xmlobj$Load(xmlfile)
-        
-xslobj.async = FALSE
-xslobj$Load(xslfile)
-
-# TRANSFORMING XML FILE
-xmlfile$transformNodeToObject(xslobj, newxmlobj)
-
-# OUTPUT RESULT TO FILE
-newxmlfile$Save(newxmlfile)
-
-xmlobj <- NULL
-xslobj <- NULL
-newxmlobj <- NULL
+# OUTPUT TO FILE
+write_xml(new_xml, "/path/to/Output.xml")
 
 print("Successfully transformed XML file!")
 
